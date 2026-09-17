b906e394dff4c4f4e161df6bf607d8f782f11e3b
swcho <swcho@mind-one.co.kr>
2026-09-17 18:20:06 +0900

    html 변환

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/portfolio-site', // 저장소 이름과 동일하게 설정
+  images: {
+    unoptimized: true, // 정적 호스팅 환경에서 이미지 최적화 오류 방지
+  },
 };
 
 export default nextConfig;
